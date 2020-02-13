141cc23c6ee58307d83c82b28f9e44187d99844c
Sam Stech <srstech@mymail.mines.edu>
2020-02-13 11:57:37 -0700

    Changed the mouth width ratio to HEAD_DIMENSION/2

--- src/Cat.java
+++ src/Cat.java
@@ -15,7 +15,7 @@ public class Cat {
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 50;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
